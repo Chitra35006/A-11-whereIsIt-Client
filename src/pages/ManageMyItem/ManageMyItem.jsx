f713abeca2194990fd337c19f8524c49f42d4942
Chitra35006 <ch18.cse@gmail.com>
2024-12-26 04:52:01 +0600

    delete my  post

--- src/pages/ManageMyItem/ManageMyItem.jsx
+++ src/pages/ManageMyItem/ManageMyItem.jsx
@@ -2,17 +2,68 @@ import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import useAuth from '../../hooks/useAuth';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const ManageMyItem = () => {
     const{user} = useAuth();
     const [posts,setPosts] = useState([]);
 
-    useEffect(()=>{
+    const fetchPost=()=>{
+        if(user?.email){
         fetch(`http://localhost:5000/myPosts?email=${user.email}`)
         .then(res => res.json())
         .then(data => setPosts(data))
-    },[user.email])
+        }
+    }
 
+    useEffect(()=>{
+        fetchPost();
+    },[user?.email])
+
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/myPosts/${id}`, {
+                    method: "DELETE",
+                })
+                    .then((res) => res.json())
+                    .then((data) => {
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your Post has been deleted.",
+                                icon: "success",
+                            });
+
+                            // After deletion, refresh the campaigns list
+                            fetchPost();
+                        } else {
+                            Swal.fire({
+                                title: "Error!",
+                                text: "Failed to delete the campaign.",
+                                icon: "error",
+                            });
+                        }
+                    })
+                    .catch((error) => {
+                        console.error("Error deleting campaign:", error);
+                        Swal.fire({
+                            title: "Error!",
+                            text: "Something went wrong.",
+                            icon: "error",
+                        });
+                    });
+            }
+        });
+    };
     
     return (
         <div className='mx-auto w-11/12'>
@@ -68,7 +119,7 @@ const ManageMyItem = () => {
               <button className="bg-orange-300 text-blue-950 font-bold px-4 py-2 rounded">
                 <Link to={`/update/${post._id}`}>UPDATE</Link>
               </button>
-              <button className="font-bold bg-gradient-to-r from-red-300 to-rose-400 text-red-900 hover:text-rose-500 px-4 py-2 rounded">
+              <button onClick={()=>handleDelete(post._id)} className="font-bold bg-gradient-to-r from-red-300 to-rose-400 text-red-900 hover:text-rose-500 px-4 py-2 rounded">
                 X
               </button>
             </div>
